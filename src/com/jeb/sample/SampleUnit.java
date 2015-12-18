fd512a24bbf7d3796c4a8aa73dc31f9d4e0bc3d3
clucas <clucas@pnfsoftware.com>
2015-12-18 16:14:40 +0100

    Add Table and Tree Documents

--- src/com/jeb/sample/SampleUnit.java
+++ src/com/jeb/sample/SampleUnit.java
@@ -21,6 +21,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.mozilla.javascript.IRFactory;
@@ -28,6 +29,7 @@ import org.mozilla.javascript.Token;
 import org.mozilla.javascript.ast.AstNode;
 import org.mozilla.javascript.ast.AstRoot;
 import org.mozilla.javascript.ast.FunctionNode;
+import org.mozilla.javascript.ast.Name;
 
 import com.pnfsoftware.jeb.core.IUnitCreator;
 import com.pnfsoftware.jeb.core.input.BytesInput;
@@ -36,7 +38,14 @@ import com.pnfsoftware.jeb.core.output.AbstractUnitRepresentation;
 import com.pnfsoftware.jeb.core.output.IGenericDocument;
 import com.pnfsoftware.jeb.core.output.IUnitFormatter;
 import com.pnfsoftware.jeb.core.output.UnitFormatterAdapter;
+import com.pnfsoftware.jeb.core.output.table.ITableDocument;
+import com.pnfsoftware.jeb.core.output.table.impl.Cell;
+import com.pnfsoftware.jeb.core.output.table.impl.StaticTableDocument;
+import com.pnfsoftware.jeb.core.output.table.impl.TableRow;
 import com.pnfsoftware.jeb.core.output.text.impl.AsciiDocument;
+import com.pnfsoftware.jeb.core.output.tree.ITreeDocument;
+import com.pnfsoftware.jeb.core.output.tree.impl.Node;
+import com.pnfsoftware.jeb.core.output.tree.impl.StaticTreeDocument;
 import com.pnfsoftware.jeb.core.properties.IPropertyDefinitionManager;
 import com.pnfsoftware.jeb.core.units.AbstractBinaryUnit;
 import com.pnfsoftware.jeb.core.units.IUnit;
@@ -89,6 +98,9 @@ public class SampleUnit extends AbstractBinaryUnit {
                 }
             }
         }
+
+        // avoid reprocessing the file
+        setProcessed(true);
         return true;
     }
 
@@ -101,17 +113,66 @@ public class SampleUnit extends AbstractBinaryUnit {
                 return new AsciiDocument(getInput());
             }
         });
-        if(functions != null) {
-            for(final FunctionNode function: functions) {
-                adapter.addDocumentPresentation(new AbstractUnitRepresentation(function.getName()) {
-                    @Override
-                    public IGenericDocument getDocument() {
-                        return new AsciiDocument(new BytesInput(function.toSource().getBytes()));
-                    }
-                });
+        // Display functions as tab
+        //        if(functions != null) {
+        //            for(final FunctionNode function: functions) {
+        //                adapter.addDocumentPresentation(new AbstractUnitRepresentation(function.getName()) {
+        //                    @Override
+        //                    public IGenericDocument getDocument() {
+        //                        return new AsciiDocument(new BytesInput(function.toSource().getBytes()));
+        //                    }
+        //                });
+        //            }
+        //        }
+
+        // Displays the Statistics table
+        adapter.addDocumentPresentation(new AbstractUnitRepresentation("Statistics") {
+            @Override
+            public IGenericDocument getDocument() {
+                return getStatisticsTable();
             }
-        }
+        });
+
+        // Display js as tree
+        adapter.addDocumentPresentation(new AbstractUnitRepresentation("Functions") {
+            @Override
+            public IGenericDocument getDocument() {
+                return getFunctionsTree();
+            }
+        });
+
         return adapter;
     }
 
+    private ITableDocument getStatisticsTable() {
+        List<TableRow> rows = new ArrayList<TableRow>();
+        rows.add(new TableRow(new Cell("Length"), new Cell(Integer.toString(root.getLength()))));
+        rows.add(new TableRow(new Cell("Type"), new Cell(Integer.toString(root.getType()))));
+        return new StaticTableDocument(Arrays.asList("Property", "Value"), rows);
+    }
+
+    private ITreeDocument getFunctionsTree() {
+        List<Node> treeRoot = new ArrayList<Node>();
+        try {
+            if(functions != null) {
+                for(FunctionNode function: functions) {
+                    treeRoot.add(buildFunctionNode(function));
+                }
+            }
+        }
+        catch(Exception e) {
+            logger.catching(e);
+        }
+        return new StaticTreeDocument(treeRoot);
+    }
+
+    private Node buildFunctionNode(FunctionNode function) {
+        Node functionNode = new Node(function.getName());
+        if(function.getParamCount() > 0) {
+            for(AstNode var: function.getParams()) {
+                functionNode.addChild(new Node(((Name)var).getIdentifier()));
+            }
+        }
+        return functionNode;
+    }
 }
--- testResources/test.js
+++ testResources/test.js
@@ -4,7 +4,7 @@ function a() {
   alert("a called");
 }
 
-function b() {
+function b(foo) {
   var b = "b called";
   alert(b);
 }
