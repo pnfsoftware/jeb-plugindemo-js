4c3f51bc267d8d91ad13f83171fe6cc52cf1189d
clucas <clucas@pnfsoftware.com>
2015-12-18 14:55:52 +0100

    Tutorial 2 Prereq

---
+++ src/com/jeb/sample/SamplePlugin.java
@@ -0,0 +1,62 @@
+/**
+Copyright PNF Software, Inc.
+
+    https://www.pnfsoftware.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package com.jeb.sample;
+
+import com.pnfsoftware.jeb.core.IPluginInformation;
+import com.pnfsoftware.jeb.core.IUnitCreator;
+import com.pnfsoftware.jeb.core.PluginInformation;
+import com.pnfsoftware.jeb.core.Version;
+import com.pnfsoftware.jeb.core.input.IInput;
+import com.pnfsoftware.jeb.core.units.AbstractUnitIdentifier;
+import com.pnfsoftware.jeb.core.units.IUnit;
+import com.pnfsoftware.jeb.core.units.IUnitProcessor;
+
+/**
+ * Entry Point of the plugin system: provides information related to Plugin and how to detect if the
+ * plugin should be used (see {@link #canIdentify(IInput, IUnitCreator)}.<br>
+ * 
+ * The {@link #prepare(String, IInput, IUnitProcessor, IUnitCreator)} method delegates the
+ * processing.
+ * 
+ * @author Cedric Lucas
+ *
+ */
+public class SamplePlugin extends AbstractUnitIdentifier {
+
+    public SamplePlugin() {
+        super("js", 0);
+    }
+
+    @Override
+    public IPluginInformation getPluginInformation() {
+        return new PluginInformation("JavaScript", "Javascript files", "PNF Software", Version.create(0, 1));
+    }
+
+    private final static byte[] JS_HEADER = "#Javascript".getBytes();
+
+    @Override
+    public boolean canIdentify(IInput input, IUnitCreator parent) {
+        return checkBytes(input, 0, JS_HEADER);
+    }
+
+    @Override
+    public IUnit prepare(String name, IInput input, IUnitProcessor unitProcessor, IUnitCreator parent) {
+        return new SampleUnit(name, input, unitProcessor, parent, pdm);
+    }
+
+}
---
+++ testResources/test.js
@@ -0,0 +1,10 @@
+#Javascript
+
+function a() {
+  alert("a called");
+}
+
+function b() {
+  var b = "b called";
+  alert(b);
+}
